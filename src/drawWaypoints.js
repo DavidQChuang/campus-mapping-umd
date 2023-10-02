fc95150335b3e695d0551b101142ef993c3c4a92
David Chuang <dc1770712@gmail.com>
2023-10-01 20:33:21 -0400

    Added precursor D* stuff

--- src/drawWaypoints.js
+++ src/drawWaypoints.js
@@ -15,6 +15,47 @@ Waypoints = {
             this.userEndpoints.splice(0, 2);
             this.pathEndpoints.splice(0, 2);
         }
+    
+        // Update/create waypoints
+        if(map.getSource('user-waypoints') == undefined) {
+            if(Waypoints.getUserEndpoints().length > 0) {
+                // Waypoint style
+                map.addSource('user-waypoints', { 
+                    "type": "geojson", "data": Waypoints.userPointFeature() });
+                map.addLayer(Layers['user-waypoints-text']);
+                map.addLayer(Layers['user-waypoints'], "user-waypoints-text");
+            }
+        } else {
+            map.getSource('user-waypoints').setData(Waypoints.userPointFeature());
+        }
+    
+        if(map.getSource('user-waypoints-path') == undefined) {
+            if(Waypoints.getUserEndpoints().length == 2) {
+                map.addSource('user-waypoints-path', {
+                    type: 'geojson', data: Waypoints.userLineFeature() });
+                map.addLayer(Layers['user-waypoints-path'], "user-waypoints");
+            }
+        } else {
+            if(Waypoints.getUserEndpoints().length == 2) {
+                map.setLayoutProperty('user-waypoints-path', 'visibility', 'visible');
+                map.getSource('user-waypoints-path').setData(Waypoints.userLineFeature());
+            } else {
+                map.setLayoutProperty('user-waypoints-path', 'visibility', 'none'); 
+            }
+        }
+    
+        if(map.getSource('path-endpoints') == undefined) {
+            if(Waypoints.getPathEndpoints().length > 0) {
+                // Waypoint style
+                console.log(Waypoints.pathPointFeature());
+                map.addSource('path-endpoints', { 
+                    "type": "geojson", "data": Waypoints.pathPointFeature() });
+                map.addLayer(Layers['path-endpoints-text']);
+                map.addLayer(Layers['path-endpoints'], "path-endpoints-text");
+            }
+        } else {
+            map.getSource('path-endpoints').setData(Waypoints.pathPointFeature());
+        }
     },
     getUserEndpoints() {
         return this.userEndpoints;
@@ -76,50 +117,17 @@ Waypoints = {
         }
     },
 };
-
-function addWaypoint(waypoint) {
-    Waypoints.addWaypoint(waypoint);
-
-    // Update/create waypoints
-    if(map.getSource('user-waypoints') == undefined) {
-        if(Waypoints.getUserEndpoints().length > 0) {
-            // Waypoint style
-            map.addSource('user-waypoints', { 
-                "type": "geojson", "data": Waypoints.userPointFeature() });
-            map.addLayer(Layers['user-waypoints-text']);
-            map.addLayer(Layers['user-waypoints'], "user-waypoints-text");
-        }
-    } else {
-        map.getSource('user-waypoints').setData(Waypoints.userPointFeature());
+Algorithms = {
+    Astar(start, goal) {
+        return pathfindAstar(start, goal, node => getDistance(goal, node))
+    },
+    Dstar(start, goal) {
+        return pathfindDstar(start, goal, node => getDistance(goal, node))
     }
+}
 
-    if(map.getSource('user-waypoints-path') == undefined) {
-        if(Waypoints.getUserEndpoints().length == 2) {
-            map.addSource('user-waypoints-path', {
-                type: 'geojson', data: Waypoints.userLineFeature() });
-            map.addLayer(Layers['user-waypoints-path'], "user-waypoints");
-        }
-    } else {
-        if(Waypoints.getUserEndpoints().length == 2) {
-            map.setLayoutProperty('user-waypoints-path', 'visibility', 'visible');
-            map.getSource('user-waypoints-path').setData(Waypoints.userLineFeature());
-        } else {
-            map.setLayoutProperty('user-waypoints-path', 'visibility', 'none'); 
-        }
-    }
-
-    if(map.getSource('path-endpoints') == undefined) {
-        if(Waypoints.getPathEndpoints().length > 0) {
-            // Waypoint style
-            console.log(Waypoints.pathPointFeature());
-            map.addSource('path-endpoints', { 
-                "type": "geojson", "data": Waypoints.pathPointFeature() });
-            map.addLayer(Layers['path-endpoints-text']);
-            map.addLayer(Layers['path-endpoints'], "path-endpoints-text");
-        }
-    } else {
-        map.getSource('path-endpoints').setData(Waypoints.pathPointFeature());
-    }
+function sleep(ms) {
+    return new Promise(r => setTimeout(r, ms));
 }
 
 function getDistance(n1, n2) {
@@ -128,7 +136,7 @@ function getDistance(n1, n2) {
     return dx*dx + dy*dy;
 }
 
-function drawWaypointRoute() {
+function drawWaypointRoute(algorithm) {
     if(Waypoints.getUserEndpoints().length != 2) {
         return;
     }
@@ -139,8 +147,7 @@ function drawWaypointRoute() {
     var start = GeoData.nodes[endpoints[0].node];
     var goal = GeoData.nodes[endpoints[1].node];
 
-    var route = pathfindAstar(start, goal,
-        node => getDistance(goal, node));
+    var route = algorithm(start, goal);
 
     if(route != false) {
         console.log("Routed: ", route);
@@ -154,6 +161,15 @@ function timeLogs(startTime, iters, tickSize) {
     return `${time}ms & ${iters} ticks, estimate: ${time - iters*tickSize}ms`;
 }
 
+////////////////////////////////////////
+//       .o.          o    
+//      .888.      `8.8.8' 
+//     .8"888.     .8'8`8. 
+//    .8' `888.       "    
+//   .88ooo8888.           
+//  .8'     `888.          
+// o88o     o8888o        
+////////////////////////////////////////
 async function pathfindAstar(start, goal, h) {
     const tickSize = 10;
     const startTime = new Date();
@@ -211,7 +227,133 @@ async function pathfindAstar(start, goal, h) {
 
         var path = getPath(curr);
         var feature = {
-            id: 'gpx-route',
+            id: 'astar-route',
+            type: 'Feature',
+            properties: {},
+            geometry: {
+                type: 'LineString',
+                coordinates: path.map(id => {
+                    var node = GeoData.nodes[id];
+                    return [ node.lon, node.lat ];
+                })
+            }
+        };
+        Draw.add(feature);
+        await sleep(tickSize);
+
+        // This is the goal. Path back using cameFrom to create the path.
+        if(curr.id == goal.id) {
+            console.log("Found goal " + curr.id + " == " + goal.id);
+            infoLabel.innerHTML = `Success in ${timeLogs(startTime, iters, tickSize)}.`;
+            return getPath(curr);
+        }
+
+        // Find neighbors by looking for adjacent nodes in ways.
+        var neighbors = []
+        for(var wayId of curr.ways) {
+            var way = GeoData.footpaths[wayId];
+
+            if(way === undefined)
+                console.log("Error: way " + wayId + " not found.", curr);
+
+            // console.log("  Found way " + wayId, way)
+            // Push adjacent nodes in way
+            for(var i = 0; i < way.nodes.length; i++) {
+                if(way.nodes[i] == curr.id) {
+                    // console.log("    Found original node. Pushing: " + way.nodes[i-1] + ", " + way.nodes[i+1] );
+                    if(i-1 >= 0)
+                        neighbors.push(way.nodes[i-1]);
+                    if(i+1 < way.nodes.length)
+                        neighbors.push(way.nodes[i+1]);
+                    break;
+                }
+            }
+        }
+        // console.log("  Found neighbors " + JSON.stringify(neighbors));
+
+        for(var neighborId of neighbors) {
+            // d(current,neighbor) is the weight of the edge from current to neighbor
+            // tentative_gScore is the distance from start to the neighbor through current
+            var neighbor = GeoData.nodes[neighborId];
+            var tempGScore = gScore[curr.id] + getDistance(curr, neighbor);
+
+            var neighborGScore = 999999999;
+            if(neighborId in gScore) {
+                neighborGScore = gScore[neighborId];
+            }
+
+            if(tempGScore < neighborGScore) {
+                // This path to neighbor is better than any previous one. Record it!
+                cameFrom[neighborId] = curr.id;
+                gScore[neighborId] = tempGScore;
+                fScore[neighborId] = tempGScore + h(neighbor);
+
+                if (!openSet.contains(neighbor, (x, y) => x.id == y.id)) {
+                    openSet.push(neighbor);
+                }
+            }
+        }
+    }
+    // Open set is empty but goal was never reached
+    infoLabel.innerHTML = `Failed in ${timeLogs(startTime, iters, tickSize)}; goal was never reached.`;
+    return false;
+}
+
+////////////////////////////////////////
+// oooooooooo.      o    
+// `888'   `Y8b  `8.8.8' 
+//  888      888 .8'8`8. 
+//  888      888    "    
+//  888      888         
+//  888     d88'         
+// o888bood8P'   
+////////////////////////////////////////
+async function pathfindDstar(start, goal, h) {
+    const tickSize = 2;
+    const startTime = new Date();
+    var infoLabel = document.getElementById('dstar-path-info');
+    infoLabel.innerHTML = "Pathing @ " + tickSize + "ms/tick";
+    var cameFrom = {}
+    
+    const getFScore = ((fScoreArr) => {
+                        return x => fScoreArr[x.id];
+                    })(fScore);
+    var openSet = new MinHeap(getFScore);
+    openSet.push(start);
+
+    const getPath = function(node) {
+        var pathNodeId = node.id;
+        var pathNodeIds = [];
+        while(pathNodeId != start.id) {
+            // console.log("Coming from " + pathNodeId +" to " + cameFrom[pathNodeId]);
+
+            pathNodeIds.push(pathNodeId);
+            pathNodeId = cameFrom[pathNodeId];
+
+            if(pathNodeId === undefined) {
+                console.log("Error")
+                throw new RangeError("Node path does not exist");
+            }
+        }
+        pathNodeIds.push(start.id);
+        return pathNodeIds;
+    }
+
+    var iters = 0;
+    while(openSet.length != 0) {
+        iters++;
+        var curr = openSet.pop();
+        // console.log("Checking node " + JSON.stringify(curr) + ", remaining: " + openSet.length);
+
+        if(openSet.length > 250) {
+            infoLabel.innerHTML = `Failed in ${timeLogs(startTime, iters, tickSize)}; search was too large.`;
+            console.log("Too long.");
+            return false;
+        }
+
+        var path = getPath(curr);
+        var feature = {
+            id: 'astar-route',
             type: 'Feature',
             properties: {},
             geometry: {
@@ -284,7 +426,7 @@ async function pathfindAstar(start, goal, h) {
 }
 
 map.on('click', (e) => {
-    addWaypoint([e.lngLat["lng"], e.lngLat["lat"]]);
+    Waypoints.addWaypoint([e.lngLat["lng"], e.lngLat["lat"]]);
     document.getElementById('waypoints-display').innerHTML = 
     "Waypoints:<br>" +
     JSON.stringify(Waypoints.getUserEndpoints())+
