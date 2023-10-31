e93123ca101d04751be83840b901edf81fcb49a1
sriasiry2 <srisairy2@gmail.com>
2023-10-31 14:41:16 -0400

    deleted comments in gpr.py

--- GPR-Testing/gpr.py
+++ GPR-Testing/gpr.py
@@ -13,8 +13,9 @@ X = []
 Y = []
 
 max_walking_speed = 8
+git_dir = os.getcwd()
 
-csv_file = pd.read_csv("/Users/shriyanssairy/Desktop/gps_csv_files/combined_file.csv")
+csv_file = pd.read_csv(f"{git_dir}/gps_csv_files/combined_file.csv")
 
 long_list = list(csv_file.Longitude)
 lat_list = list(csv_file.Latitude)
@@ -74,68 +75,12 @@ Xp, Yp = np.meshgrid(x1, x2, indexing='ij')
 Zp = np.reshape(y_pred,(steps,steps))
 
 
-surf = ax.plot_surface(Xp, Yp, Zp, rstride=1, cstride=1, cmap='jet', linewidth=0, antialiased=False)
 plt.xlabel("longitude")
 plt.ylabel("latitude")
+surf = ax.plot_surface(Xp, Yp, Zp, rstride=1, cstride=1, cmap='jet', linewidth=0, antialiased=False)
 surf1 = ax2.pcolormesh(Xp, Yp, Zp, cmap='jet')
 ax2.set_aspect("equal")
 fig.set_size_inches(12,7)
 surf2 = ax.scatter(X[:,0], X[:,1], Y, linewidth=0, antialiased=False)
 # fig.colorbar(surf, shrink=0.5, aspect=5 )
 plt.show()
-
-
-
-
-'''
-----------------------------------------------------------
-'''
-
-# X_train, Y_train = X, Y
-
-# kernel = C(1.0, (1e-3, 1e3)) * RBF([5,5], (1e-2, 1e2))
-# gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=15)
-# gp.fit(X, Y)
-# y_pred, MSE = gp.predict(x, return_std=True)
-# # model.fit(Y_train, Z_train)
-
-# fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-
-# X,Y = np.meshgrid(X,Y)
-
-# # Plot the surface.
-# surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
-#                        linewidth=1, antialiased=False)
-
-# # Customize the z axis.
-# ax.set_zlim(0, 3)
-# ax.set_xlim(38.9850,38.9959)
-# ax.set_ylim(-76.9444, -76.9320)
-# ax.zaxis.set_major_locator(LinearLocator(10))
-# # A StrMethodFormatter is used automatically
-# ax.zaxis.set_major_formatter('{x:.02f}')
-
-# Add a color bar which maps values to colors.
-# fig.colorbar(surf, shrink=0.5, aspect=5)
-
-# plt.show()
-
-
-'''
-----------------------------------------------------------
-'''
-
-# plt.scatter([x_value], [predicted_y], label=r"predicted point")
-# plt.plot(X, mean_prediction, label="Mean prediction")
-# plt.fill_between(
-#     X.ravel(),
-#     mean_prediction - 1.96 * std_prediction,
-#     mean_prediction + 1.96 * std_prediction,
-#     alpha=0.5,
-#     label=r"95% confidence interval",
-# )
-# plt.legend()
-# plt.xlabel("$x$")
-# plt.ylabel("$f(x)$")
-# _ = plt.title("Gaussian process regression on noise-free dataset")
-
